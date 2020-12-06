fb75a16511f24689b74ff7d7daca0695b923e144
kaleidoscopica <pouncekitti@gmail.com>
2020-12-06 17:33:20 -0600

    Completed day 6 part 1

---
+++ Day6/6-1.py
@@ -0,0 +1,23 @@
+import csv
+
+def main():
+
+    customs_questions = []
+    count = 0
+
+    with open('input.txt') as file:
+        customs_questions = file.read()
+
+    customs_questions = customs_questions.split("\n\n") #creates a separate item in the list 
+    customs_questions = [item.replace('\n', '') for item in customs_questions] #merges single newlines together
+
+    for item in customs_questions:
+        tmp_list = []
+        for char in item:
+            if char not in tmp_list:
+                tmp_list.append(char)
+        count += len(tmp_list)
+
+    print(count)
+
+main()
